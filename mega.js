99b33d004bc028d8367e0f507bbc2aac8a01da7e
Team Wolfmare Official <teamwolfmareofficial@gmail.com>
2025-03-20 19:17:25 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-  email: "Enter your mega email",
-  password: "Enter your mega password",
+  email: "hansakafreco@gmail.com",
+  password: "fuckyou1234/.*js *",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
